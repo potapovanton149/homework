f9e6d7f64186a721d4146773dfa19da9299c0803
Potapov Anton <exchialle@gmail.com>
2025-03-08 17:59:58 +0400

    Реализация задания №1 из модуля №6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Я родился!");
+        Point point1 = new Point(1, 3);
+        Point point2 = new Point(1, 3);
+        Point point3 = new Point(5, 8);
+
+        System.out.println("Проверка инициализации объектов: \n" +
+                point1 + "\n" +
+                point2 + "\n" +
+                point3 + "\n"
+        );
+        System.out.printf("Результат сранения по ссылке:" +
+                        "\n 1 и 2 точка = %b" +
+                        "\n 2 и 3 точка = %b",
+                point1 == point2, point2 == point3);
+
+        System.out.printf("\n\nРезультат сранения объектов:" +
+                        "\n 1 и 2 точка = %b" +
+                        "\n 2 и 3 точка = %b",
+                point1.equals(point2), point2.equals(point3));
     }
 }
