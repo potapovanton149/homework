320854b7f760fc3825aeb179493a9b12b9b72223
Potapov Anton <exchialle@gmail.com>
2025-03-10 15:20:19 +0400

    Внесение правок в реализацию задания № 4 из модуля №6 (см. комментарий класса PolyLine)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,31 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.SortedMap;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(1, 5);
-        Point point2 = new Point(2, 8);
-        Point point3 = new Point(5, 3);
-        Point point4 = new Point(8, 9);
+        List<Point> pointList = new ArrayList<Point>();
 
-        PolyLine polyLine = new PolyLine();
+        pointList.add(new Point(1, 5));
+        pointList.add(new Point(2, 8));
+        pointList.add(new Point(5, 3));
+        pointList.add(new Point(8, 9));
 
-        polyLine.pointList.add(point1);
-        polyLine.pointList.add(point2);
-        polyLine.pointList.add(point3);
-        polyLine.pointList.add(point4);
+        PolyLine polyLine = new PolyLine(pointList);
+
+
 
         System.out.println("1.Создать Ломаную, проходящую через точки {1;5}, {2;8}, {5;3}, {8,9}:\n" + polyLine);
 
-
         System.out.println("\n\n\n2. Рассчитать длину Ломаной: \n" + polyLine.getLength());
-
 
         System.out.println("\n\n\n3. Получить у Ломаной массив Линий:\n" + Arrays.toString(polyLine.getLines()));
 
 
-        Point[] arrayPoints = polyLine.getLines();
+        Line[] arrayPoints = polyLine.getLines();
         double lengthArrayPoints = 0;
-        for (int i = 0; i < arrayPoints.length - 1; i++) {
-            double deltaX = arrayPoints[i + 1].getX() - arrayPoints[i].getX();
-            double deltaY = arrayPoints[i + 1].getY() - arrayPoints[i].getY();
-            lengthArrayPoints += Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        for (int i = 0; i < arrayPoints.length; i++) {
+           lengthArrayPoints += arrayPoints[i].getLenght();
         }
         System.out.println("\n\n\n4. Рассчитать длину массива Линий:\n" + lengthArrayPoints);
 
@@ -37,8 +33,8 @@ public class Main {
         System.out.println("\n\n\n5. Сравнить длину Ломаной и массива Линий:\n" + (lengthArrayPoints == polyLine.getLength()));
 
 
-        point2.setX(12);
+        polyLine.lineList.get(0).getEndLine().setX(12);
         System.out.println(String.format("\n\n\n6. Изменить координаты Точки {2,8} таким образом, чтобы она стала иметь значение {12,8}. \n\n" +
-                        "Точка %s \nЛоманая %s \nМассив %s", point2, polyLine, Arrays.toString(polyLine.getLines())));
+                        "Точка %s \nЛоманая %s \nМассив %s", polyLine.lineList.get(0).getEndLine().getX(), polyLine, Arrays.toString(polyLine.getLines())));
     }
 }
--- src/main/java/PolyLine.java
+++ src/main/java/PolyLine.java
@@ -1,29 +1,43 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
+//Что нужно исправить:
+//1. Объект должен содержать поле лист линий, а не точек
+//2. Добавить конструктор класса если передан лист точек
+//3. Переиспользовать код из класса Line при реализации метода расчета длины ломаной линии
+
 public class PolyLine {
-   List<Point> pointList;
+   List<Line> lineList;
 
     PolyLine() {
-        pointList = new ArrayList<>();
+        lineList = new ArrayList<>();
     }
 
-    public Point[] getLines() {
-        Point[] arrayPoints = new Point[pointList.size()];
+    PolyLine(List<Point> listPoint){
+        lineList = new ArrayList<>();
 
-        for (int i = 0; i < pointList.size(); i++) {
-            arrayPoints[i] = pointList.get(i);
+        for (int i = 0; i < listPoint.size() - 1; i++) {
+            Line line = new Line(listPoint.get(i), listPoint.get(i + 1));
+            lineList.add(line);
         }
-        return arrayPoints;
+    }
+
+    public Line[] getLines() {
+        Line[] arrayLines = lineList.toArray(Line[]::new);
+
+        for (int i = 0; i < lineList.size(); i++) {
+            arrayLines[i] = lineList.get(i);
+        }
+        return arrayLines;
     }
 
     public double getLength() {
         double res = 0;
         Line line = new Line(0, 0, 0, 0);
 
-        for (int i = 0; i < pointList.size() - 1; i++) {
-            line.setStartLine(pointList.get(i));
-            line.setEndLine(pointList.get(i+1));
+        for (int i = 0; i < lineList.size(); i++) {
+            line = lineList.get(i);
             res += line.getLenght();
         }
         return res;
@@ -33,11 +47,13 @@ public class PolyLine {
     public String toString() {
         String res = "Линия [";
         int i = 1;
-        for (Point point : pointList){
-            res += "Точка " +
-                    i + String.format(" {%d, %d}, ",
-                    point.getX(),
-                    point.getY());
+        for (Line line : lineList){
+            res += "Линия " +
+                    i + String.format(" {%d, %d : %d, %d}, ",
+                    line.getStartLine().getX(),
+                    line.getStartLine().getY(),
+                    line.getEndLine().getX(),
+                    line.getEndLine().getY());
             i++;
         }
         return res;
