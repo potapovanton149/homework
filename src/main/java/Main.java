9f48c17d8770d4537c83153395134dad01a628b5
Potapov Anton <exchialle@gmail.com>
2025-03-23 21:58:26 +0400

    Реализаяция задания №3 из модуля №8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.SortedMap;
 
 public class Main {
+    public static double totalLengthLine(List<Measurable> Measurable) {
+        double totalLengthLine = 0;
+        for (Measurable measurable : Measurable) {
+            totalLengthLine += measurable.getLength();
+        }
+        return totalLengthLine;
+    }
+
     public static void main(String[] args) {
         List<Point> pointList = new ArrayList<Point>();
 
@@ -12,9 +21,20 @@ public class Main {
 
         Point[] pointsArray = pointList.toArray(new Point[0]);
 
+        PolyLine polyLine = new PolyLine().fromPoint(pointList);
         ClosedPolyLine closedPolyLine = new ClosedPolyLine(pointsArray);
+
+        List<Measurable> measurables = new ArrayList<>();
+        measurables.add(polyLine);
+        measurables.add(closedPolyLine);
+
 
         System.out.println("Создаем замкнутую линию, проходящую через точки {1;5}, {2;8}, {5;3}, {8,9}:\n" + closedPolyLine);
         System.out.println("\nРассчитываем длину замкнутой линии: \n" + closedPolyLine.getLength());
+
+        System.out.println("\n\n\nСоздаем ломаную линию, проходящую через точки {1;5}, {2;8}, {5;3}, {8,9}:\n" + polyLine);
+        System.out.println("\nРассчитываем длину ломаной линии: \n" + polyLine.getLength());
+
+        System.out.println("\n\n\nСумма длины всех объектов:\n" + Main.totalLengthLine(measurables));
     }
 }
