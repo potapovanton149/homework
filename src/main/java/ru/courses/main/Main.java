e3b4a38e89e0c652b9bc0b73c43f52103b07a182
Potapov Anton <exchialle@gmail.com>
2025-03-29 02:02:20 +0400

    Реализация задания №4 первой части из модуля №9

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,19 +1,34 @@
 package ru.courses.main;
 
 import ru.courses.geometry.Line;
-import ru.courses.geometry.Point2D;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Line line1 = new Line(1,2,3,4);
-        Line line2 = new Line(1, 2, 3, 4);
+        Line line1 = new Line(1, 1, 1, 1);
+        Line line2 = new Line(2, 2, 2, 2);
+        Line line3 = new Line(3, 3, 3, 3);
 
-        System.out.println(line1.equals(line2));
-        System.out.println(line1.hashCode() == line2.hashCode());
+        List<Line> lineList1 = new ArrayList<>();
+        lineList1.add(line1);
+        lineList1.add(line2);
+        lineList1.add(line3);
 
-        Line line3 = line1.clone();
 
-        System.out.println(line1.equals(line3));
-        System.out.println(line1.hashCode() == line3.hashCode());
+        Line line4 = new Line(1, 1, 1, 1);
+        Line line5 = new Line(2, 2, 2, 2);
+        Line line6 = new Line(3, 3, 3, 3);
+
+        List<Line> lineList2 = new ArrayList<>();
+        lineList2.add(line4);
+        lineList2.add(line5);
+        lineList2.add(line6);
+
+        System.out.println(lineList1);
+        System.out.println(lineList2);
+        System.out.println(lineList1.equals(lineList2));
+        System.out.println(lineList1.hashCode() == lineList2.hashCode());
     }
 }
