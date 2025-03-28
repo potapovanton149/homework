0d1569ea72a3fd25f8d4564232764c6c1e55fbdb
Potapov Anton <exchialle@gmail.com>
2025-03-29 00:29:15 +0400

    Реализация задания №3 первой части из модуля №9

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,14 +1,19 @@
 package ru.courses.main;
 
+import ru.courses.geometry.Line;
 import ru.courses.geometry.Point2D;
 
 public class Main {
     public static void main(String[] args) {
-        Point2D point1 = new Point2D(1, 2);
-        Point2D point2 = point1.clone();
+        Line line1 = new Line(1,2,3,4);
+        Line line2 = new Line(1, 2, 3, 4);
 
+        System.out.println(line1.equals(line2));
+        System.out.println(line1.hashCode() == line2.hashCode());
 
-        System.out.println(point1.hashCode() == point2.hashCode());
-        System.out.println(point1.equals(point2));
+        Line line3 = line1.clone();
+
+        System.out.println(line1.equals(line3));
+        System.out.println(line1.hashCode() == line3.hashCode());
     }
 }
