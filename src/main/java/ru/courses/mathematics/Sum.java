e4c3ffa5f0434a10ebed647f363252606809db9d
Potapov Anton <exchialle@gmail.com>
2025-03-30 23:11:47 +0400

    Реализация задания №2 третий части из модуля №9

--- src/main/java/ru/courses/mathematics/Sum.java
+++ src/main/java/ru/courses/mathematics/Sum.java
@@ -1,23 +1,24 @@
 package ru.courses.mathematics;
 
+import java.util.Scanner;
+
 public class Sum {
-    public static void main(String[] args) throws NumberFormatException {
+    public static void main(String[] args) {
         double x = 0;
         double y = 0;
-
         int count = 0;
 
         for (String string : args) {
-            try {
-                if (count == 0) {
-                    x = Double.parseDouble(string);
-                    count++;
-                } else if (count == 1) {
-                    y = Double.parseDouble(string);
-                    break;
-                }
-            } catch (NumberFormatException ex) {
-                System.out.println(ex.getMessage());
+            if (!string.matches("^[+-]?(\\d+\\.?\\d*|\\.\\d+)([eE][+-]?\\d+)?$")){
+                continue;
+            }
+
+            if (count == 0) {
+                x = Double.parseDouble(string);
+                count++;
+            } else if (count == 1) {
+                y = Double.parseDouble(string);
+                break;
             }
         }
         System.out.println(x + y);
